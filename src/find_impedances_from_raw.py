c83f706fab29e720ab5ef80f4e37e5a42e10f517
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2022-03-09 12:35:08 +0100

    find channels with impedance above threshold

---
+++ src/find_impedances_from_raw.py
@@ -0,0 +1,21 @@
+""" find impendances above threshold (10kohm)"""
+
+import pandas as pd
+from mne.io import read_raw_brainvision
+
+ids_map = pd.read_excel('docs/ids_map.xlsx', header=1, index_col='behavioral_id')
+ids_map = ids_map.loc[:2132614].drop_duplicates('bids_id')
+subject_ids = ids_map.reset_index().eeg_id
+bids = ids_map.reset_index().bids_id
+
+violated_thresh = {}
+for sub, bid in zip(subject_ids, bids):
+    raw = read_raw_brainvision(f'data/Live Sessions/{sub}.vhdr', misc=['ECG'])
+    ch_names = raw.ch_names
+    violated_thresh[bid] = [dict({ch_nam: raw.impedances[ch_nam]['imp']})
+                            for ch_nam in ch_names
+                            if raw.impedances[ch_nam]['imp'] >= 11]
+
+
+df = pd.DataFrame.from_dict(violated_thresh, orient='index')
+df.to_csv('docs/violated_thresh.csv')
