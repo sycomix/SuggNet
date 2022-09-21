91f00de744a7428f671550938ade224d6caeef73
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2022-09-21 16:35:06 +0200

    Inspect hypnotist's eeg data.

--- src/validation/1. mark_bad_channels.py
+++ src/validation/1. mark_bad_channels.py
@@ -9,13 +9,13 @@ import pickle
 # initiate an empty dictionary to collect the name of bad channels in.
 bads = {}
 
-for subject_path in Path('data/raw_test').glob('*.vhdr'):
+for subject_path in Path('data/raw/PLB_HYP_Hypnotist_data').glob('*.vhdr'):
     subject_id = subject_path.stem
 
     raw = mne.io.read_raw_brainvision(subject_path, eog=('EOG1', 'EOG2'), misc=['ECG'])
     raw.set_channel_types({'ECG': 'ecg'})
 
-    raw.plot(n_channels=61, remove_dc=True, highpass=1, lowpass=40, filtorder=0)
+    raw.plot(n_channels=65, remove_dc=True, highpass=1, lowpass=40, filtorder=0)
     plt.show()
 
     bad = raw.info['bads']
@@ -23,5 +23,13 @@ for subject_path in Path('data/raw_test').glob('*.vhdr'):
     bads[subject_id] = bad
 
 # pickle bad channels
-with open("bad_channels.pkl", "wb") as f:
-    pickle.dump(bads, f)
+# with open("bad_channels.pkl", "wb") as f:
+#     pickle.dump(bads, f)
+
+# # visualize hypnotist's EEG recordings
+# path = 'data/raw/PLB_HYP_Hypnotist_data/Relaxation.vhdr'
+# raw = mne.io.read_raw_brainvision(path, eog=('EOG1', 'EOG2'), misc=['ECG'])
+# raw.set_channel_types({'ECG': 'ecg'})
+
+# raw.plot(n_channels=61, remove_dc=True, highpass=1, lowpass=40, filtorder=0)
+# plt.show()
